02ccf8344fd44adc2d9613c6ce9e1e584edc534f
tdharris <tylerdavidharris@gmail.com>
2015-03-27 11:28:57 -0600

    removed unnecessary files

--- views/snippets/styles.php
+++ views/snippets/styles.php
@@ -1,7 +1,6 @@
 <!-- Yahoo will deliver each of the small css files in a single request by using the & symbol like below -->
 <link rel="stylesheet" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&amp;pure/0.6.0/pure-min.css&amp;pure/0.6.0/grids-responsive-min.css">
   
-<link rel="stylesheet" href="css/code-themes/dreamweaver.css">
 <link rel="stylesheet" href="css/animate.css">
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css"/>
 <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick-theme.css"/>
