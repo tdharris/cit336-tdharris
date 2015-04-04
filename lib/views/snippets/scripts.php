3d84dd4a659b5b57fc270cf6d4d5eba1eb2a1cb2
tdharris <tylerdavidharris@gmail.com>
2015-04-04 15:09:36 -0600

    tidy up

--- lib/views/snippets/scripts.php
+++ lib/views/snippets/scripts.php
@@ -1,18 +1,27 @@
 <!-- JS -->
-<script type="text/javascript" src="public/js/rainbow-custom.min.js"></script>
-
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script type="text/javascript" src="public/js/mixitup.min.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.min.js"></script>
-<script type="text/javascript" src="public/js/spin.min.js"></script>
-<script type="text/javascript" src="public/js/sendMail.js"></script>
+
+	<!-- Dependencies -->
+	<?php if ($_SERVER[REQUEST_URI] == '/?action=q&url=contact') {?>
+		<script type="text/javascript" src="public/js/sendMail.js"></script>
+	<?php };?>
+
+	<?php if (strpos($_SERVER[REQUEST_URI], '/?action=q&project=') !== false || $_SERVER[REQUEST_URI] == '/?action=q&url=portfolio') {?>
+		<script type="text/javascript" src="public/lib/rainbow/rainbow-custom.min.js"></script>
+		<script type="text/javascript" src="public/lib/slickjs/slick.min.js"></script>
+		<script type="text/javascript" src="public/lib/mixitup/mixitup.min.js"></script>
+		<script type="text/javascript" src="public/js/tdharris.js"></script>
+	<?php };?>
+
+	<?php if ($_SERVER[REQUEST_URI] == '/?action=q&url=contact' || $_SERVER[REQUEST_URI] == '/?action=a&url=admin') {?>
+		<script type="text/javascript" src="public/lib/spin/spin.min.js"></script>
+		<script type="text/javascript" src="public/lib/toastr/toastr.min.js"></script>
+	<?php };?>
+
+	<?php if ($_SERVER[REQUEST_URI] == '/?action=a&url=admin') {?>
+	  <script type="text/javascript" src="public/lib/dropzone/dropzone.js"></script>
+	  <script type="text/javascript" src="public/js/admin.js"></script>
+	<?php };?>
+
 <script type="text/javascript" src="public/js/login.js"></script>
-<script type="text/javascript" src="public/js/toastr.min.js"></script>
-
-<?php if ($_SERVER[REQUEST_URI] == '/?action=a&url=admin') {?>
-  <script type="text/javascript" src="public/js/dropzone.js"></script>
-  <script type="text/javascript" src="public/js/admin.js"></script>
-<?php };?>
-
 <script type="text/javascript" src="public/js/getProject.js"></script>
-<script type="text/javascript" src="public/js/tdharris.js"></script>
--- lib/views/snippets/styles.php
+++ lib/views/snippets/styles.php
@@ -1,16 +1,25 @@
 <!-- Yahoo will deliver each of the small css files in a single request by using the & symbol like below -->
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&amp;pure/0.6.0/pure-min.css&amp;pure/0.6.0/grids-responsive-min.css">
   
-<link rel="stylesheet" type="text/css" href="public/css/animate.css">
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css"/>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick-theme.css"/>
-<link rel="stylesheet" type="text/css" href="public/css/toastr.min.css"/>
-<link rel="stylesheet" type="text/css" href="public/css/dropzone.css"/>
+<link rel="stylesheet" type="text/css" href="public/lib/animate/animate.css">
 <link rel="stylesheet" type="text/css" href="public/css/aside.css"/>
 <link rel="stylesheet" type="text/css" href="public/css/home.css"/>
 
 <?php if ($_SERVER[REQUEST_URI] == '/') {?>
   <link rel="stylesheet" type="text/css" href="public/css/big.css"/> 
+<?php };?>
+
+<?php if (strpos($_SERVER[REQUEST_URI], '/?action=q&project=') !== false || $_SERVER[REQUEST_URI] == '/?action=q&url=portfolio') {?>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.4.1/slick-theme.css"/>
+<?php };?>
+
+<?php if ($_SERVER[REQUEST_URI] == '/?action=q&url=contact' || $_SERVER[REQUEST_URI] == '/?action=a&url=admin') {?>
+	<link rel="stylesheet" type="text/css" href="public/lib/toastr/toastr.min.css"/>
+<?php };?>
+
+<?php if ($_SERVER[REQUEST_URI] == '/?action=a&url=admin') {?>
+	<link rel="stylesheet" type="text/css" href="public/lib/dropzone/dropzone.css"/>
 <?php };?>
 
 <!-- Custom Fonts -->
